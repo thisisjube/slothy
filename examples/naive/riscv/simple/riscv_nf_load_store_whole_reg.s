44904611ccb2a9e56ba09815fbb041098ac144e8
Amin Abdulrahman <amin@abdulrahman.de>
2025-08-19 16:22:58 +0200

    Add support for LMUL >=1 * Allow allocating register ranges following LMUL mandates * For instructions using <nf> specifier, merge parts of the logic with the one for LMUL * Add tests and examples

---
+++ examples/naive/riscv/simple/riscv_nf_load_store_whole_reg.s
@@ -0,0 +1,11 @@
+start:
+vl1r.v     v8, (x1)
+vl2r.v     v10, (x2)
+vl4re32.v  v12, (x3) 
+vl8r.v     v16, (x4)
+
+vs1r.v     v24, (x5)
+vs2r.v     v26, (x6)
+vs4re32.v  v28, (x7)
+vs8r.v     v0, (x8)
+end:
